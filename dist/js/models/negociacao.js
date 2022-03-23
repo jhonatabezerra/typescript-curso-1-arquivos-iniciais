941581199028acdbbaf13c08c69a77012e6c7545
Jhonata Ramos Bezerra <jhonata.empresa@gmail.com>
2022-03-22 23:50:42 -0300

    implementado getters

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -8,9 +8,26 @@ export class Negociacao {
         this.#quantidade = quantidade;
         this.#valor = valor;
     }
+    get data() {
+        return this.#data;
+    }
+
+    get quantidade() {
+        return this.#quantidade;
+    }
+
+    get valor() {
+        return this.#valor;
+    }
+
+    get volume() {
+        return this.#quantidade * this.#valor;
+    }
 }
+
+
 
 /* NOTES:
     - O export define que esta classe pode ser exportada para um modulo.
-    - Isso é uma implementação usando ECMASCRIPT;
+    - Isso é uma implementação usando modulos do ECMASCRIPT;
 */
